9101b5523bac552609a5f3a05f6bdd293ff2c447
Keith Moss <keithamoss@gmail.com>
2018-06-10 18:46:53 +0800

    Ignoring more stuff

--- backend/db.php
+++ backend/db.php
@@ -6,7 +6,7 @@ date_default_timezone_set("Australia/Perth");
 // require_once "modules/stalls.php";
 
 // Create (connect to) SQLite database in file
-$file_db = new PDO('sqlite:../db/demsausage.sqlite3');
+$file_db = new PDO('sqlite:../db/scremsong.sqlite3');
 $file_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 function failForAPI($msg = "Failed for an unknown reason. Oh dear, Master Luke!") {
