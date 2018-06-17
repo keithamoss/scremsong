4489029c1df1638298904bf688107bd5d16a75e3
Keith Moss <keithamoss@gmail.com>
2018-06-17 14:53:20 +0800

    Foo

--- backend/do-test.py
+++ backend/do-test.py
@@ -1,4 +1,6 @@
 import subprocess
+from os import listdir
+from os.path import isfile, join
 
 # filepath = "some/complete/path/to/file"
 # hostname = "user@remote_system"
@@ -6,7 +8,23 @@ import subprocess
 
 subprocess.call(['pwd'])
 subprocess.call(['ls'])
-print(subprocess.Popen(['scp', "-i ./travis_digitalocean", "-o StrictHostKeyChecking=no", "-rp", "root@209.97.166.142:/scremsong/app/build/", "./"]).wait())
-print(subprocess.call(['ls']))
-print(subprocess.call(['ls', "buld"]))
+subprocess.Popen(['scp', "-i ./travis_digitalocean", "-o StrictHostKeyChecking=no", "-rp", "root@188.166.231.42:/scremsong/logs/", "./"]).wait()
+print(">> scp done")
+print()
+
+subprocess.call(['ls'])
+print(">> ls done")
+print()
+
+subprocess.call(['ls', 'logs'])
+print(">> ls logs done")
+print()
+
+mypath = "/scremsong/logs/"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+print(onlyfiles)
+print(">> file listing donne")
+print()
+
+# print(subprocess.call(['ls', "buld"]))
 # scp -i ./travis_digitalocean -o StrictHostKeyChecking=no -rp root@209.97.166.142:/scremsong/app/build/ ./
