89c6bc7ca7913013bfe0839dabc0c56806d441b2
Keith Moss <keithamoss@gmail.com>
2018-06-20 21:02:50 +0800

    Testing...

--- backend/do-test.py
+++ backend/do-test.py
@@ -6,47 +6,43 @@ from os.path import isfile, join
 # hostname = "user@remote_system"
 # remote_path = "some/remote/path"
 
-out = subprocess.check_output(['pwd'])
-print(out)
-print(">> pwd done")
-print()
+# out = subprocess.check_output(['pwd'])
+# print(out)
+# print(">> pwd done")
+# print()
 
-out = subprocess.check_output(['ls'])
-print(out)
-print(">> ls done")
-print()
+# out = subprocess.check_output(['ls'])
+# print(out)
+# print(">> ls done")
+# print()
 
-out = subprocess.check_output(['scp', "-i ./travis_digitalocean", "-o StrictHostKeyChecking=no", "-rp", "root@139.59.251.69:/scremsong/logs/", "./"])
-print(out)
+subprocess.check_output(["scp", "-i ./deploy_key", "-o StrictHostKeyChecking=no", "-rp", "root@139.59.251.69:/scremsong/logs/nginx/", "./logs/nginx/"])
 print(">> scp done")
 print()
 
-out = subprocess.check_output(['ls'])
-print(out)
-print(">> ls done")
-print()
+# out = subprocess.check_output(['ls'])
+# print(out)
+# print(">> ls done")
+# print()
 
-out = subprocess.check_output(['ls', './logs'])
-print(out)
-print(">> ls logs done")
-print()
+# out = subprocess.check_output(['ls', './logs'])
+# print(out)
+# print(">> ls logs done")
+# print()
 
-mypath = "./logs"
+mypath = "./logs/nginx/"
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 print(onlyfiles)
 print(">> file listing donne")
 print()
 
-with open("./logs/error.log") as f:
-    print(f.read())
-    print(">> error.log")
-    print()
+# with open("./logs/error.log") as f:
+#     print(f.read())
+#     print(">> error.log")
+#     print()
 
 
-with open("./logs/access.log") as f:
-    print(f.read())
-    print(">> access.log")
-    print()
-
-# print(subprocess.call(['ls', "buld"]))
-# scp -i ./travis_digitalocean -o StrictHostKeyChecking=no -rp root@209.97.166.142:/scremsong/app/build/ ./
+# with open("./logs/access.log") as f:
+#     print(f.read())
+#     print(">> access.log")
+#     print()
