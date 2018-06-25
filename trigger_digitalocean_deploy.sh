ba40ed2b0c8b87fa20dc200074c9b90ade178130
Keith Moss <keithamoss@gmail.com>
2018-06-25 16:54:06 +0800

    Abc

--- trigger_digitalocean_deploy.sh
+++ trigger_digitalocean_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo $TRAVIS_COMMIT
 echo $TRAVIS_COMMIT_MESSAGE
-# curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_API_KEY" -d '{"request": {"branch": "master", "message": "From Scremsong: $TRAVIS_COMMIT - $TRAVIS_COMMIT_MESSAGE"}}' https://api.travis-ci.org/repo/keithamoss%2Fdigitalocean-stack/requests
+echo $NOT_TRAVIS_API_KEY
+curl -s -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $NOT_TRAVIS_API_KEY" -d '{"request": {"branch": "master", "message": "From Scremsong: $TRAVIS_COMMIT - $TRAVIS_COMMIT_MESSAGE"}}' https://api.travis-ci.org/repo/keithamoss%2Fdigitalocean-stack/requests
