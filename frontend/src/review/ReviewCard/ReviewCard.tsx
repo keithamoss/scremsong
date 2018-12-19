7263d51b5b46fbc02a4b077ae0ae92d80cc23c83
Keith Moss <keithamoss@gmail.com>
2018-12-19 19:41:00 +0800

    Queue users can now take simple Twitter web intent actions from their queue
    For #30

--- frontend/src/review/ReviewCard/ReviewCard.tsx
+++ frontend/src/review/ReviewCard/ReviewCard.tsx
@@ -1,9 +1,10 @@
-import { Button, Card, CardActions, CardContent, Collapse, Theme, withStyles, WithStyles } from "@material-ui/core"
+import { Button, Card, CardActions, CardContent, Collapse, IconButton, Theme, withStyles, WithStyles } from "@material-ui/core"
 import AssignmentTurnedIn from "@material-ui/icons/AssignmentTurnedIn"
 import classNames from "classnames"
 import * as React from "react"
 import { ExitHandler } from "react-transition-group/Transition"
 import Tweet from "react-tweet"
+import { LikeIcon, ReplyIcon, RetweetIcon } from "../../aseets/TwitterIcons"
 import { IReviewerAssignment } from "../../redux/modules/reviewers"
 import { ISocialTweetList } from "../../redux/modules/social"
 
@@ -53,13 +54,35 @@ class ReviewCard extends React.PureComponent<TComponentProps, IState> {
     public render() {
         const { assignment, tweets, classes } = this.props
 
+        const replyToTweetLink = (props: any) => (
+            <a href={`https://twitter.com/intent/tweet?in_reply_to=${assignment.social_id}`} {...props} target="_blank" />
+        )
+        const likeTweetLink = (props: any) => (
+            <a href={`https://twitter.com/intent/like?tweet_id=${assignment.social_id}`} {...props} target="_blank" />
+        )
+        const retweetTweetLink = (props: any) => (
+            <a href={`https://twitter.com/intent/retweet?tweet_id=${assignment.social_id}`} {...props} target="_blank" />
+        )
+
         return (
             <Collapse in={this.state.shown} onExited={this.onMarkAsDone}>
                 <Card className={classes.paddedCard}>
                     <CardContent>
                         <Tweet data={tweets[assignment.social_id].data} />
                     </CardContent>
-                    <CardActions>
+                    <CardActions disableActionSpacing={true}>
+                        <IconButton disableRipple={true} component={replyToTweetLink}>
+                            <ReplyIcon />
+                        </IconButton>
+
+                        <IconButton disableRipple={true} component={likeTweetLink}>
+                            <LikeIcon />
+                        </IconButton>
+
+                        <IconButton disableRipple={true} component={retweetTweetLink}>
+                            <RetweetIcon />
+                        </IconButton>
+
                         <Button color={"primary"} variant="contained" className={classes.button} onClick={this.handleChange}>
                             <AssignmentTurnedIn className={classNames(classes.leftIcon, classes.iconSmall)} />
                             Mark as done
