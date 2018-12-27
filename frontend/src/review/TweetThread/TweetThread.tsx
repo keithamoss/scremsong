9637b419317518f97e1ef504e188ff02368e29cb
Keith Moss <keithamoss@gmail.com>
2018-12-27 21:06:05 +0800

    Reddit Boost-style nested comment colours for assigned tweets

--- frontend/src/review/TweetThread/TweetThread.tsx
+++ frontend/src/review/TweetThread/TweetThread.tsx
@@ -12,6 +12,37 @@ export interface IProps {
 
 export interface IState {}
 
+// @FIXME Use props in Material UI style when that ships
+const getColurForDepth = (depth: number) => {
+    switch (depth) {
+        case 0:
+            return "lightgrey"
+        case 1:
+            return "rgb(160, 0, 67)" // dark red
+        case 2:
+            return "rgb(214, 62, 81)" // bright red
+        case 3:
+            return "rgb(245, 108, 66)" // orange
+        case 4:
+            return "rgb(252, 173, 98)" // bright orange
+        case 5:
+            return "rgb(255, 224, 139)" // orangey yellow
+        case 6:
+            return "rgb(232, 246, 152)" // pale yellow
+        case 7:
+            return "rgb(171, 222, 163)" // pale green
+        case 8:
+            return "rgb(105, 194, 166)" // green
+        case 9:
+            return "rgb(53, 135, 189)" // blue
+        default:
+            return "rgb(95, 79, 162)" // purple
+    }
+}
+const getBorderCSSForDepth = (depth: number) => {
+    return depth > 0 ? `6px solid ${getColurForDepth(depth)}` : "0px"
+}
+
 type TComponentProps = IProps
 class TweetThread extends React.PureComponent<TComponentProps, IState> {
     public render() {
@@ -33,7 +64,7 @@ class TweetThread extends React.PureComponent<TComponentProps, IState> {
                     <Tweet data={tweets[tweetId].data} linkProps={{ target: "_blank", rel: "noreferrer" }} />
                 )}
                 {!(ourDepth === 0 && childTweetIds.length === 0) && (
-                    <div style={{ marginLeft: ourDepth * 10, borderLeft: "6px solid lightgrey" }}>
+                    <div style={{ marginLeft: ourDepth * 6, borderLeft: getBorderCSSForDepth(ourDepth) }}>
                         <Tweet data={tweets[tweetId].data} linkProps={{ target: "_blank", rel: "noreferrer" }} />
                     </div>
                 )}
