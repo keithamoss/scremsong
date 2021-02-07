9ed122f4adc77b19e2b84326d06c0adff1756227
Keith Moss <keithamoss@gmail.com>
2021-02-07 15:44:23 +0800

    Retire frontend

--- frontend/src/@types/index.d.ts
+++
@@ -1,5 +0,0 @@
-// Modules without TypeScript definitions get defined here
-declare module "dot-prop-immutable"
-declare module "react-tweet"
-declare module "reconnecting-websocket"
-declare module "jss-material-ui"
--- frontend/src/polyfills.tsx
+++
@@ -1,8 +0,0 @@
-// Import any specific polyfills required for legacy browsers
-import "core-js/modules/es.array.find"
-import "core-js/modules/es.array.find-index"
-import "core-js/modules/es.object.keys"
-import "core-js/modules/es.promise"
-import "core-js/modules/es.array.includes"
-import "core-js/modules/es.object.entries"
-import "core-js/modules/es.object.values"
