84dc676cccbca38aa79a92a4292725d84d4ad8b9
Keith Moss <keithamoss@gmail.com>
2021-03-17 19:53:08 +0800

    Setting Celery concurrency to 6 (from 4) to try to work around duplicate task_fill_missing_tweets() sitting around hogging active slots in the queue

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -35,9 +35,9 @@ if [ "$1" = "celery_worker" ]; then
     # Print all executed commands to the terminal
     set -x
 
-    # Concurrency: 1 for streaming, 1 for backfill + processing tweets, 1 for Twitter rate limit collection, and 1 for solely processing tweets
-    exec celery -A scremsong worker -n scremworker@%h -l info --concurrency=4 --pool gevent
-    # exec celery -A scremsong worker -n scremworker@%h -l info --concurrency=4 --logfile=logs/celery-worker.log
+    # Concurrency: 1 for streaming, 1 for Twitter rate limit collection, 1 for tweet backfill + processing tweets, and 3 for solely processing tweets
+    exec celery -A scremsong worker -n scremworker@%h -l info --concurrency=6 --pool gevent
+    # exec celery -A scremsong worker -n scremworker@%h -l info --concurrency=6 --logfile=logs/celery-worker.log
     exit
 fi
 
